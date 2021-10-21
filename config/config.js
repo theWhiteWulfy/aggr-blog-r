1ceda2fc7e5d724bbebf4a9c6e2e84467e009cee
Alok Prateek <i@alokprateek.in>
2021-10-21 18:30:31 +0530

    INIT

---
+++ config/config.js
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+/*  company-name ElderGagaroth
+    module-name gagarath
+*/    
+
+module.exports = {
+    title: "ElderGagaroth",
+    titleTemplate: "ElderGagaroth - React MTG",
+    description:
+        "ElderGagaroth is ElderGagaroth",
+    author: "ElderGagaroth",
+    twitterUsername: "@ElderGagaroth",
+    keywords: ["bootstrap", "html", "react", "gatsby", "template", "blog", "creative"],
+    image: "banner_image.png",
+    siteUrl: "https://ElderGagaroth.com",
+    pathPrefix: "/bootstrap-template",
+    localUrl: "http://localhost:8000",
+    canonical: "https://ElderGagaroth.com",
+    social: {
+        facebook: "https://www.facebook.com/ElderGagaroth",
+        twitter: "https://twitter.com/ElderGagaroth",
+        linkedin: "https://www.linkedin.com/company/ElderGagaroth/",
+    },
+    siteLanguage: "en",
+    favicon: "src/assets/images/favicon.png", // Used for manifest favicon generation
+    shortName: "ElderGagaroth", // shortname for manifest. *Must* be shorter than 12 characters
+    themeColor: "#5E60E7",
+    backgroundColor: "#fff",
+};
---
+++ src/container/about-us/gagarath-history/index.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import { graphql, useStaticQuery } from "gatsby";
+import HistoryItem from '@components/history-item'
+import {
+    GagarathHistoryWrap,
+    GagarathRow,
+    GagarathCol6,
+    GagarathHistoryTitle,
+} from './style'
+
+const GagarathHistoryArea = () => {
+    const gagarathHistoryAreaQery = useStaticQuery(graphql`
+        query GagarathHistoryAreaQery {
+            aboutUsJson(id: { eq: "about-us-pages" }) {
+                id
+                history_title
+                history {
+                    id
+                    title
+                    decText
+                }
+            }
+        }
+    `);
+    const {
+        history_title,
+        history
+    } = gagarathHistoryAreaQery.aboutUsJson;
+
+    return (
+        <GagarathHistoryWrap>
+            <Container>
+                <Row>
+                    <Col>
+                        <GagarathRow>
+                            <GagarathCol6>
+                                <GagarathHistoryTitle dangerouslySetInnerHTML={{ __html: history_title }}/>
+                            </GagarathCol6>
+                            <GagarathCol6>
+                                {history.map((itemData) => (
+                                    <HistoryItem
+                                        key={itemData.id}
+                                        title={itemData.title}
+                                        decText={itemData.decText}
+                                    />
+                                ))}
+                            </GagarathCol6>
+                        </GagarathRow>
+                    </Col>
+                </Row>
+            </Container>
+        </GagarathHistoryWrap>
+    )
+}
+
+
+export default GagarathHistoryArea
